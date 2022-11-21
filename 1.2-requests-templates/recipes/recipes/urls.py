adc5ede9a45d5d718a77f815f3c86abb694418b7
Alina Vorontsova <vorontsova-alina@mail.ru>
2022-11-21 21:46:41 +0300

    urls.py update

--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 
 from django.urls import path
+from calculator.views import home_view, recipe
 
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('', home_view, name='home'),
+    path('<recipe>/', recipe, name='recipe'),
 ]
