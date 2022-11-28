10fb1858074e40e02aa752ca0045e15b67e96064
Alina Vorontsova <vorontsova-alina@mail.ru>
2022-11-28 13:23:26 +0300

    admin.py update

--- 2.1-databases/work_with_database/phones/admin.py
+++ 2.1-databases/work_with_database/phones/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Phone
+
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'image']
+    list_dilter = ['name', 'price']
